91dadcb3f2e9cfc058cc5afc6780ffc5c62c72ce
sintiavm <marin.sintiavallet@gmail.com>
2021-05-15 13:24:41 -0500

    ling

--- Lesson4/JS/time.js
+++ Lesson4/JS/time.js
@@ -2,3 +2,7 @@ let updateLast = document.lastModified
 document.getElementById('lastUpdate').innerText = updateLast
 let fullYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = fullYear;
+
+function toggleMenu()  {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
