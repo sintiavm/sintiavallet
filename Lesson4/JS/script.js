3083e021847a090261273b44906e5919adc5fc24
sintiavm <marin.sintiavallet@gmail.com>
2021-05-15 21:04:26 -0500

    hide

---
+++ Lesson4/JS/script.js
@@ -0,0 +1,3 @@
+function toggleMenu()  {
+    document.getElementById("newNav").classList.toggle("hide");
+}
--- Lesson4/JS/time.js
+++ Lesson4/JS/time.js
@@ -3,6 +3,3 @@ document.getElementById('lastUpdate').innerText = updateLast
 let fullYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = fullYear;
 
-function toggleMenu()  {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
