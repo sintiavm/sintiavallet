aae5f3a7e56e1de8d61353ea76d45711905468e2
sintiavm <marin.sintiavallet@gmail.com>
2021-06-05 13:20:31 -0500

    html

--- Lesson7/JS/lazyload.js
+++ Lesson7/JS/lazyload.js
@@ -1,6 +1,6 @@
 
 //get all imgs with dataa -src atribute
-const iamgesToLoad = document.querySelectorAll("{.content}")
+const imagesToLoad = document.querySelectorAll("{.content}")
 let imagesToLoad = document.querySelectorAll('img[.content]');
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('.content'));
@@ -12,3 +12,20 @@ imagesToLoad.forEach((img) => {
     loadImages(img);
   });
   
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  } 
