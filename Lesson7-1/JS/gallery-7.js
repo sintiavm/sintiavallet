aedbc80edff7fb894a18e78e23a35e8a42110636
sintiavm <marin.sintiavallet@gmail.com>
2021-06-05 00:12:51 -0500

    html

---
+++ Lesson7-1/JS/gallery-7.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('td').hover( function () {
+    
+     $(this).animate({opacity:'0'});
+       },
+    
+        function () {
+    
+        $(this).animate({opacity:'0'});
+        }); 
+    });
+    
