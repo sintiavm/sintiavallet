ac816a8d792b88af13ac327837d02563c1001e32
sintiavm <marin.sintiavallet@gmail.com>
2021-06-18 23:06:01 -0500

    small

--- Lesson9/js/home-wireframes.js
+++ Lesson9/js/home-wireframes.js
@@ -5,7 +5,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-  const prophets = jsonObject['prophets'];
+  const towns = jsonObject['towns'];
     console.table(jsonObject);  // temporary checking for valid response and data parsing
   for (let i = 0; i < prophets.length; i++ ) {
     let card = document.createElement('section');
@@ -15,10 +15,10 @@ fetch(requestURL)
     let image = document.createElement('img');
     
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-p1.textContent = "Date of Birth:" + ' ' + prophets[i].birthdate;
-p2.textContent = "Place of Birth:" + ' ' + prophets[i].birthplace;
-image.setAttribute('src', prophets[i].imageurl, 'alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + prophets[i].order);
+h2.textContent = towns[i].name + ' ' + towns[i].lastname;
+p1.textContent = "Date of Birth:" + ' ' + towns[i].birthdate;
+p2.textContent = "Place of Birth:" + ' ' + towns[i].birthplace;
+image.setAttribute('src', images/${wheather2.photo});
 card.setAttribute('class', "card");
 
 
@@ -30,3 +30,45 @@ card.appendChild(image);
 document.querySelector('div.cards').appendChild(card);
   }
   });
+  $(document).ready(function(){
+    $('td').hover( function () {
+    
+     $(this).animate({opacity:'0'});
+       },
+    
+        function () {
+    
+        $(this).animate({opacity:'0'});
+        }); 
+    });
+    
+    //get all imgs with dataa -src atribute
+const imagesToLoad = document.querySelectorAll("{img}")
+let imagesToLoad = document.querySelectorAll('img]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('img'));
+  image.onload = () => {
+    image.removeAttribute('.img');
+  };
+};
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+  
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  } 
