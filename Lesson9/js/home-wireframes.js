9405c4c7add55c057b85d9eefa904d0136f27bed
sintiavm <marin.sintiavallet@gmail.com>
2021-06-19 23:02:11 -0500

    csss

--- Lesson9/js/home-wireframes.js
+++ Lesson9/js/home-wireframes.js
@@ -5,70 +5,49 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-  const towns = jsonObject['towns'];
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-  for (let i = 0; i < prophets.length; i++ ) {
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let image = document.createElement('img');
-    
-
-h2.textContent = towns[i].name + ' ' + towns[i].lastname;
-p1.textContent = "Date of Birth:" + ' ' + towns[i].birthdate;
-p2.textContent = "Place of Birth:" + ' ' + towns[i].birthplace;
-image.setAttribute('src', images/${wheather2.photo});
-card.setAttribute('class', "card");
+    const towns = jsonObject['towns'];
+    console.table(jsonObject);
+    // temporary checking for valid response and data parsing
 
 
-card.appendChild(h2);
-card.appendChild(p1);
-card.appendChild(p2);
-card.appendChild(image);
 
-document.querySelector('div.cards').appendChild(card);
-  }
+
+    for (let i = 0; i < towns.length; i++) {
+      if(towns[i].name == 'Fish Haven' || towns[i].name == 'Soda Springs' || towns[i].name == 'Preston'){
+
+      
+
+
+      let card = document.createElement('section');
+      let h3 = document.createElement('h3');
+      let h4 = document.createElement('h4');
+      let p1 = document.createElement('p');
+      let p2 = document.createElement('p');
+      let p3 = document.createElement('p');
+      let image = document.createElement('img');
+
+
+      card.className = "town-card" + i;
+      h3.textContent = towns[i].name;
+      h4.textContent = towns[i].motto;
+      p1.textContent = "Year Founded:" + ' ' + towns[i].yearFounded;
+      p2.textContent = "Population:" + ' ' + towns[i].currentPopulation;
+      p3.textContent = "Anual Rainfall:" + ' ' + towns[i].averageRainfall;
+      image.setAttribute('src', 'images/' + towns[i].photo);
+      image.apphend = towns[i].photo;
+      card.setAttribute('class', "card");
+
+
+      card.appendChild(h3);
+      card.appendChild(h4);
+      card.appendChild(p1);
+      card.appendChild(p2);
+      card.appendChild(p3);
+      card.appendChild(image);
+      card.appendChild(image);
+      card.appendChild(image);
+
+      document.querySelector('div.cards').appendChild(card);
+    }
+    }
   });
-  $(document).ready(function(){
-    $('td').hover( function () {
-    
-     $(this).animate({opacity:'0'});
-       },
-    
-        function () {
-    
-        $(this).animate({opacity:'0'});
-        }); 
-    });
-    
-    //get all imgs with dataa -src atribute
-const imagesToLoad = document.querySelectorAll("{img}")
-let imagesToLoad = document.querySelectorAll('img]');
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('img'));
-  image.onload = () => {
-    image.removeAttribute('.img');
-  };
-};
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-  
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  } 
