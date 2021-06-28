3d7f5ef6c5a2223b50454bb9f557f4af6c6dc4c6
sintiavm <marin.sintiavallet@gmail.com>
2021-06-28 17:16:21 -0500

    css

--- Lesson10/js/weatherapi.js
+++ Lesson10/js/weatherapi.js
@@ -7,4 +7,18 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    const temperture = document.querySelector('#temperture');
+    temperture.textContent = jsObject.main.temp.tofixef(0);
+    const currenly = document.querySelector('#currenly');
+    const desc = jsObject.weather[0].description;
+    currenly.innerHTML = <stong>${desc.toUpperCase()</stong>;
+
+
+      // images
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
   }); 
