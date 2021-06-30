fb33081b7fb7e31f568f13e6ce7217bb04080895
sintiavm <marin.sintiavallet@gmail.com>
2021-06-30 18:11:06 -0500

    js

--- Lesson10/js/weatherapi.js
+++ Lesson10/js/weatherapi.js
@@ -1,26 +1,30 @@
-const cityid ="5604473";
-const APPID ="d9e0c4ec7e97714a178f177b3aff8cb9";
+const cityid = "5604473";
+const APPID = "d9e0c4ec7e97714a178f177b3aff8cb9";
 
 
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&appid=${APPID}&units=imperial`;
 fetch(apiURL)
   .then((response) => response.json())
   .then(response => {
-    console.log(response);   
-    document.getElementById('temparture').textContent=response.main.temp_max;
-    
-// let temperture = document.querySelector('#temperture');
- //  let weather = response.main.temp.toFixed(0);
-  //  const currenly = document.querySelector('#currenly');
-   // const desc = response.weather[0].description;
-    //currenly.innerHTML = <stong>${desc.toUpperCase()</strong>;
+    console.log(response);
+    // document.getElementById('temparture').textContent=response.main.temp_max;
+
+    let temperture = response.main.temp_max;
+    let speed = response.wind.speed;
+    const desc = response.weather[0].description;
+    const des = response.main.humidity;
 
 
-      // images
+    document.getElementById('Currently').innerText = desc;
+    document.getElementById('High').innerHTML = temperture + '&deg;F';
+    document.getElementById('Humidity').innerHTML = des;
+    document.getElementById('Speed').innerHTML = speed;
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + response.weather[0].icon + '.png';  // note the concatenation
-  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
-  }) 
+    // images
+
+    //const imagesrc = 'https://openweathermap.org/img/w/' + response.weather[0].icon + '.png';  // note the concatenation
+    // note how we reference the weather array
+    //document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    //document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    //document.getElementById('icon').setAttribute('alt', desc);
+  })
