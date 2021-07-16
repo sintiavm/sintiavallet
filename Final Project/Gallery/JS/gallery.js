2d31b294239629e3491531058463c50e7ef347a4
sintiavm <marin.sintiavallet@gmail.com>
2021-07-15 23:29:05 -0500

    final

---
+++ Final Project/Gallery/JS/gallery.js
@@ -0,0 +1,31 @@
+ 
+  //get all imgs with dataa -src atribute
+const imagesToLoad = document.querySelectorAll("{img}");
+let imagesToLoad = document.querySelectorAll("{img}");
+const loadImages = (image) => {
+image.setAttribute('src', image.getAttribute("{img}"));
+image.onload = () => {
+  image.removeAttribute("{img}");
+};
+};
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
